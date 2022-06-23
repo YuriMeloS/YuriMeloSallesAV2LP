f1cfb58724e251e72aae7730a09ed6e6e1a492ec
YuriMeloS <65675262+YuriMeloS@users.noreply.github.com>
2022-06-22 22:08:26 -0300

    Add files via upload

--- ProvaAV2LP/src/Questao2/Questao2.java
+++ ProvaAV2LP/src/Questao2/Questao2.java
@@ -7,7 +7,17 @@ import java.util.Iterator;
 public class Questao2 {
 
 	public static void main(String[] args) {
-
+		 int array[][] = {{1, 2}, {1, 2}};
+	        int array1[][] = {{3, 4}, {3, 4}};
+	        int l = array.length;
+	        for (int i = 0; i < l; i++) {
+	            for (int j = 0; j < 2; j++) {
+	                System.out.print(" " + array[i][j]);
+	            }
+	            System.out.println();
+	        }
+			
+		
 	}
 
 }
